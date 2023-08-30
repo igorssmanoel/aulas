794b91f6af3e1264f02d9825e75cf215c26f0fe9
Igor Manoel <igorssmanoel@gmail.com>
2023-08-30 20:55:24 -0300

    Update last exercise

--- hello/script.js
+++ hello/script.js
@@ -1,9 +1,9 @@
 const colors = ['blue', 'yellow', 'purple', 'pink', 'green', 'darkblue', 'lightblue', 'lightgreen', 'grey', 'black']
 
-for (let i = 1; i <= 3; i++) {
+/* for (let i = 1; i <= 3; i++) {
     let box = document.getElementById("box-" + i);
     box.style.background = colors[i - 1];
-}
+} */
 
 function changeColor(id) {
     let box = document.getElementById(id);
@@ -12,13 +12,21 @@ function changeColor(id) {
 
 
 function init() {
-    let boxes = document.getElementsByClassName("box");
+    const quantity = 13;
+    /* let boxes = document.getElementsByClassName("box");
 
     Object.values(boxes).forEach((item, index) => {
         console.log(item, index);
         console.log(item.id);
         item.onclick = () => changeColor(item.id);
-    })
+    }) */
+    let container = document.getElementById("container");
+    let box = document.createElement("div");
+    box.className = "box";
+    box.id = 'box-1';
+    box.innerText = 1;
+    container.appendChild(box);
+
 }
 
 init()
